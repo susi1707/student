e2d538fa835a61a85e6f98268e3f3a3e7e30fccd
susi1707 <razorblack17@gmail.com>
2022-09-21 19:21:20 +0530

    list of students

--- student/src/main/java/org/rb/student/Controller/StudentController.java
+++ student/src/main/java/org/rb/student/Controller/StudentController.java
@@ -1,7 +1,21 @@
 package org.rb.student.Controller;
 
+import org.rb.student.Service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class StudentController {
+
+    @Autowired
+    private StudentService service;
+
+
+    @RequestMapping("/")
+    public String listStudent(Model model){
+        model.addAttribute("list_student",service.getAllStudent());
+        return "student";
+    }
 }
--- student/src/main/java/org/rb/student/Service/StudentService.java
+++ student/src/main/java/org/rb/student/Service/StudentService.java
@@ -1,4 +1,18 @@
 package org.rb.student.Service;
 
+import org.rb.student.Entity.Student;
+import org.rb.student.Repository.StudentRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class StudentService {
+
+    @Autowired
+    private StudentRepo repo;
+    public List<Student> getAllStudent(){
+        return repo.findAll();
+    }
 }
