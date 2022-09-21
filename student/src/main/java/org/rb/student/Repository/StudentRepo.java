91b4a3e58319fd549fbdc12abb1c1e0bf35aad78
susi1707 <razorblack17@gmail.com>
2022-09-21 18:19:37 +0530

    Entity and repo added

--- student/src/main/java/org/rb/student/Repository/StudentRepo.java
+++ student/src/main/java/org/rb/student/Repository/StudentRepo.java
@@ -1,4 +1,7 @@
 package org.rb.student.Repository;
 
-public interface StudentRepo {
+import org.rb.student.Entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepo extends JpaRepository<Student,Long> {
 }
