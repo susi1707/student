0cda8143c79feef3816134f8c6f90d3ed3c5703b
susi1707 <razorblack17@gmail.com>
2022-09-21 17:47:11 +0530

    new

---
+++ student/src/test/java/org/rb/student/StudentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rb.student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
